03121766f7a3a696ad266cb3ba0d221c666f3120
Pei <pei.cheng@triton>
2013-12-07 18:41:20 +0800

    autoimport monkeyjump v1.3.88

--- MonkeyJump/MonkeyJump/main.m
+++ MonkeyJump/MonkeyJump/main.m
@@ -11,7 +11,9 @@
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
+#if defined (__STELLA_VERSION_MAX_ALLOWED)
+    [UIScreen mainScreen].currentMode   = [UIScreen mainScreen].iPhone3GEmulationMode;
+#endif
     int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
     [pool release];
     return retVal;
