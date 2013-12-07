d022efd0a9c8818c5444a13023b550e69ed24510
Pei <pei.cheng@triton>
2013-12-07 18:41:21 +0800

    autoimport gles2sample v1.3.88

--- GLES2sample/main.m
+++ GLES2sample/main.m
@@ -48,6 +48,9 @@
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+#if defined (__STELLA_VERSION_MAX_ALLOWED)
+    [UIScreen mainScreen].currentMode   = [UIScreen mainScreen].iPhone3GEmulationMode;
+#endif
 
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
